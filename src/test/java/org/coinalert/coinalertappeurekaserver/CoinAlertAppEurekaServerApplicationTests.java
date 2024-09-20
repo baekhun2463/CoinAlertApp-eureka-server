0166958d4c8e4e7379cba6c2f3ae0373041cf67c
baekhun <baekhun2463@gmail.com>
2024-09-20 13:03:10 +0900

    init

---
+++ src/test/java/org/coinalert/coinalertappeurekaserver/CoinAlertAppEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAlertAppEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
